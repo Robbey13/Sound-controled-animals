9fa7619781101a56acf9b1bd4cd8f302bfc9e3b2
Robbey13 <86802239+Robbey13@users.noreply.github.com>
2022-10-16 11:25:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function startClassification()
+function StartClassification()
 {
     navigator.mediaDevices.getUserMedia({audio: true});
     classifier = ml5.soundclassifier('https://teachablemachine.withgoogle.com/models/LpLs5ZlwW/model.json',modelReady);
@@ -6,4 +6,46 @@ function startClassification()
 
 function modelReady(){
     classifier.classify(gotResults);
+}
+
+function gotResults(error,results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+    
+        document.getElementById("result_label").innerHTML = 'I can hear - '+
+            results[0].label;
+        document.getElementById("result_confidence").innerHTML= 'Accuracy - '+
+            (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color = "rgb("
+        +random_number_r+ ","+random_number_g+ ","+random_number_b+")"
+        document.getElementById("result_confidence").style.color = "rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")"
+        
+        img = document.getElementById('');
+        img1 = document.getElementById('');
+        img2 = document.getElementById('');
+        img3 = document.getElementById('');
+        
+        if (results[0].label == "Cluck"){
+            img.src = 'chicken.png';
+            
+        } else if (results[0].label == "Meow") {
+             img.src = 'cat.png' ;
+            
+        } else if (results[0].label == "Moo") {
+             img.src = 'cow.png';
+            
+        } else if (results[0].label == "Oink"){
+             img.src = 'pig.png';
+           
+  
+        
+     
+        }
+    }
 }
